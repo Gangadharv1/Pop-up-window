29f05449a627ec25b4660d3550e16110e931d6c6
Gangadhar Vananje <86451843+Gangadharv1@users.noreply.github.com>
2024-12-19 20:53:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+ document.getElementById('open').addEventListener('click', function() {
+    document.getElementById('modal_container').style.display = 'flex';
+  });
+  
+  document.getElementById('close').addEventListener('click', function() {
+    document.getElementById('modal_container').style.display = 'none';
+  });
+  
+  document.getElementById('close_button').addEventListener('click', function() {
+    document.getElementById('modal_container').style.display = 'none';
+  });
+
